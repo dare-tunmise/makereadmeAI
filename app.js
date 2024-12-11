54ba72fcd360c01bb63f11e6c6ab917bb016ded3
dare-tunmise <daretunmise@yahoo.com>
2024-12-11 19:12:35 +0100

    configured open ai api key

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=> {
+    res.send("we're live!")
+})
+
+
+app.listen(port, ()=> {
+    console.log(`app is listening at http;//localhost:${port}`);
+})
